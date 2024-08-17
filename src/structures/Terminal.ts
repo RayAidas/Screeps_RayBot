f3a02b4bfd5be82899cba9f00fd05c20980e861f
ChenFeng <cf_acfun@163.com>
2024-08-17 22:33:34 +0800

    fix: 修复一直买power的BUG

--- src/structures/Terminal.ts
+++ src/structures/Terminal.ts
@@ -39,7 +39,7 @@ export default class Terminal extends Singleton {
         }
         // TODO 有BUG待修复，下单失败的情况也return了，导致后续的订单没有机会下单
         if (Game.time % (terminal.room.memory.index + 20) == 0) {
-            if (terminal.store.power < 10000) {
+            if (terminal.room.storage.store.power < 10000) {
                 global.autoDeal(terminal.room.name, 'power', 1200);
                 return;
             }
